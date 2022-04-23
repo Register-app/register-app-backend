45477af233f173eec36de122ab2a04b9251a759d
B1AL1 <k.konrad9902@gmail.com>
2022-04-23 19:06:08 +0200

    added basic stuff for student

---
+++ src/main/java/com/bialy/registerapi/controller/StudentController.java
@@ -0,0 +1,32 @@
+package com.bialy.registerapi.controller;
+
+import com.bialy.registerapi.model.Student;
+import com.bialy.registerapi.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        return studentService.getStudents();
+    }
+
+    @GetMapping("/students/{id}")
+    public Student getStudent(@PathVariable Long id){
+        throw new IllegalArgumentException("Not implemented yet");
+    }
+}
